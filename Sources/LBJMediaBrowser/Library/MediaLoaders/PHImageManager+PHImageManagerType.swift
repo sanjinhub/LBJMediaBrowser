656f756e173f04c2ca00d3be00fbb6b59520fef4
SanJin <sanjinhub@gmail.com>
2024-05-19 02:52:03 +0800

    Update PHImageManager+PHImageManagerType.swift

--- Sources/LBJMediaBrowser/Library/MediaLoaders/PHImageManager+PHImageManagerType.swift
+++ Sources/LBJMediaBrowser/Library/MediaLoaders/PHImageManager+PHImageManagerType.swift
@@ -2,65 +2,55 @@ import Photos
 import UIKit
 
 extension PHImageManager: PHImageManagerType {
+    func requestImage(
+        for asset: PHAsset,
+        targetSize: CGSize,
+        contentMode: PHImageContentMode,
+        options: PHImageRequestOptions?,
+        completion: @escaping (Result<UIImage, Error>) -> Void
+    ) -> PHImageRequestID {
+        requestImage(
+            for: asset,
+            targetSize: targetSize,
+            contentMode: contentMode,
+            options: options
+        ) { image, _ in
 
-  func requestImage(
-    for asset: PHAsset,
-    targetSize: CGSize,
-    contentMode: PHImageContentMode,
-    options: PHImageRequestOptions?,
-    completion: @escaping (Result<UIImage, Error>) -> Void
-  ) -> PHImageRequestID {
-
-    requestImage(
-      for: asset,
-      targetSize: targetSize,
-      contentMode: contentMode,
-      options: options
-    ) { image, info in
-
-      if let image = image {
-        completion(.success(image))
-      } else if let error = info?[PHImageErrorKey] as? Error {
-        completion(.failure(error))
-      } else {
-        completion(.failure(NSError.unknownError))
-      }
+            if let image = image {
+                completion(.success(image))
+            }
+        }
     }
-  }
 
-  func requestImageData(
-    for asset: PHAsset,
-    options: PHImageRequestOptions?,
-    completion: @escaping (Result<Data, Error>) -> Void
-  ) -> PHImageRequestID {
-    requestImageDataAndOrientation(for: asset, options: options) { imageData, _, _, info in
-      if let imageData = imageData {
-        completion(.success(imageData))
-      } else if let error = info?[PHImageErrorKey] as? Error {
-        completion(.failure(error))
-      } else {
-        completion(.failure(NSError.unknownError))
-      }
+    func requestImageData(
+        for asset: PHAsset,
+        options: PHImageRequestOptions?,
+        completion: @escaping (Result<Data, Error>) -> Void
+    ) -> PHImageRequestID {
+        requestImageDataAndOrientation(for: asset, options: options) { imageData, _, _, info in
+            if let imageData = imageData {
+                completion(.success(imageData))
+            } else if let error = info?[PHImageErrorKey] as? Error {
+                completion(.failure(error))
+            } else {}
+        }
     }
-  }
 
-  func requestAVAsset(
-    forVideo asset: PHAsset,
-    options: PHVideoRequestOptions?,
-    completion: @escaping (Result<URL, Error>) -> Void
-  ) -> PHImageRequestID {
-    requestAVAsset(
-      forVideo: asset,
-      options: options
-    ) { asset, _, info in
+    func requestAVAsset(
+        forVideo asset: PHAsset,
+        options: PHVideoRequestOptions?,
+        completion: @escaping (Result<URL, Error>) -> Void
+    ) -> PHImageRequestID {
+        requestAVAsset(
+            forVideo: asset,
+            options: options
+        ) { asset, _, info in
 
-      if let urlAsset = asset as? AVURLAsset {
-        completion(.success(urlAsset.url))
-      } else if let error = info?[PHImageErrorKey] as? Error {
-        completion(.failure(error))
-      } else {
-        completion(.failure(NSError.unknownError))
-      }
+            if let urlAsset = asset as? AVURLAsset {
+                completion(.success(urlAsset.url))
+            } else if let error = info?[PHImageErrorKey] as? Error {
+                completion(.failure(error))
+            } else {}
+        }
     }
-  }
 }
